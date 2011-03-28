5948dfffb664e295e8b8d09186d39a6d4dd12e82
Martijn Pieters <mj@jarn.com>
2011-03-28 22:18:21 +0200

    Start a new package for Plone-Celery integration

---
+++ README.txt
@@ -0,0 +1,15 @@
+Introduction
+============
+
+This package integrates the `celery`_ distributed task queue into Plone. It
+allows you to configure celery from your zope.conf configuration file, and
+make sending task messages honor the Zope transaction manager.
+
+Credits
+=======
+
+Design and implementation: `Martijn Pieters`_, `Jarn AS`_
+
+.. _`celery`: http://celeryproject.org/
+.. _`Martijn Pieters`: mailto:mj@jarn.com
+.. _`Jarn AS`: http://jarn.com
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='plone.app.celery',
+      version=version,
+      description="Celery integration for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='celery, plone, distributed, task queue',
+      author='Jarn AS',
+      author_email='info@jarn.com',
+      url='http://pypi.python.org/pypi/plone.app.celery',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
